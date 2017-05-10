6aef101f5c549c062f8cb89a55bdb9ce35cde985
rakesh kumar <rakeshmaurya262@gmail.com>
2017-05-10 23:56:49 +0530

    search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,8 +17,13 @@ class WelcomeController < ApplicationController
   def search_candidate
    # user_ids =  UserJob.all.select{|j| (j.job_ids & current_user.jobs.map(&:id)).any?}.map(&:user_id).uniq
    # @users =  User.where("first_name LIKE ? AND role = ?","#{params[:search_candidate]}%", "candidate")
+   
     if params[:salary_aearch].present?
-      if params[:salary_aearch] < "60000"
+      if  params[:salary_aearch] == "30000"
+        a = "0"   
+        obj = params[:salary_aearch].to_s
+        
+      elsif  params[:salary_aearch] == "60000"
         a = "0"   
         obj = params[:salary_aearch].to_s
       else
@@ -29,8 +34,32 @@ class WelcomeController < ApplicationController
       a = "0"
       obj = User.maximum(:salary_expectation)
     end
+
+    if params[:search_candidate].present?
+      @users =  User.where(role: 'candidate').where("lower(first_name) LIKE ? AND lower(salary_expectation) BETWEEN ? AND ?","#{params[:search_candidate].downcase}%", a, obj)
+
+    elsif params[:location_search].present?
+      @users =  User.where(role: 'candidate').where("lower(current_location) LIKE ? AND lower(salary_expectation) BETWEEN ? AND ?", a, obj)
+
+    elsif  params[:salary_aearch].present?
+      @users =  User.where(role: 'candidate').where("lower(salary_expectation) BETWEEN ? AND ?", a, obj)
+
+    elsif params[:search_candidate].present? && params[:location_search].present? && params[:salary_aearch].present? 
+      @users =  User.where("lower(first_name) LIKE ? AND role = ? AND lower(current_location) LIKE ? AND lower(salary_expectation) BETWEEN ? AND ?","#{params[:search_candidate].downcase}%", "candidate", "#{params[:location_search].downcase}%", a, obj)
+
+    elsif params[:search_candidate].present? && params[:location_search].present?
+      @users =  User.where("lower(first_name) LIKE ? AND role = ? AND lower(current_location) LIKE ?","#{params[:search_candidate].downcase}%", "candidate", "#{params[:location_search].downcase}%")
+
+    elsif params[:location_search].present? && params[:salary_aearch].present? 
+      @users =  User.where(role: 'candidate').where("lower(current_location) LIKE ? AND lower(salary_expectation) BETWEEN ? AND ?", "#{params[:location_search].downcase}%", a, obj)
+
+
+    elsif params[:search_candidate].present? && params[:salary_aearch].present? 
+      @users =  User.where("lower(first_name) LIKE ? AND role = ? AND lower(salary_expectation) BETWEEN ? AND ?","#{params[:search_candidate].downcase}%", "candidate", a, obj)
+    else
+      @users = User.where(role: 'candidate')
+    end
    
-    @users =  User.where("lower(first_name) LIKE ? AND role = ? AND lower(current_location) LIKE ? AND lower(salary_expectation) BETWEEN ? AND ?","#{params[:search_candidate].downcase}%", "candidate", "#{params[:location_search].downcase}%", a, obj)
   end
 end
 
