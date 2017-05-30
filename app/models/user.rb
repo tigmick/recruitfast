ca6b7be3d41763a8cee847bab286597b4356c544
rakesh kumar <rakeshmaurya262@gmail.com>
2017-05-31 02:07:22 +0530

    commented client approval process

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,14 +23,14 @@ class User < ActiveRecord::Base
     first_name+" "+last_name
   end
 
-  def active_for_authentication?
-    # Uncomment the below debug statement to view the properties of the returned self model values.
-    # logger.debug self.to_yaml
-    if self.role == "client" && self.status == false
-      false
-    else
-      super
-    end
-  end
+  # def active_for_authentication?
+  #   # Uncomment the below debug statement to view the properties of the returned self model values.
+  #   # logger.debug self.to_yaml
+  #   if self.role == "client" && self.status == false
+  #     false
+  #   else
+  #     super
+  #   end
+  # end
 
 end
